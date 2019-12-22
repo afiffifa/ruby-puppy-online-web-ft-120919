1622f60915ee7e87520a02b084bd795059ec03b3
Muhammad Afif Alaptagin <afiffifa89@gmail.com>
2019-12-22 19:34:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,21 @@
 class Dog
-  attr_accessor :name
 
-  @@all = []
+	attr_accessor :name
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+	@@all = []
 
-  def self.clear_all
-    @@all.clear
-  end
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
 
-  def self.all
-    @@all.each do |d|
-      puts d.name
-    end
-  end
+	def self.clear_all
+		@@all.clear
+	end
+
+	def self.all
+		@@all.each { |dog_name| puts dog_name.name }
+	end
+
+
 end
