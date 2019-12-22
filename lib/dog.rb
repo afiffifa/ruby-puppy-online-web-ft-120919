8b44f4ab93b18ee8e54fb21d42ea801c0b3daded
Muhammad Afif Alaptagin <afiffifa89@gmail.com>
2019-12-22 19:30:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,4 @@
-# Add your code here
 class Dog
-
   attr_accessor :name
 
   @@all = []
@@ -10,14 +8,13 @@ class Dog
     @@all << self
   end
 
-  def self.all
-    @@all.each do |dog|
-      puts dog.name
-    end
-  end
-
   def self.clear_all
     @@all.clear
   end
 
+  def self.all
+    @@all.each do |d|
+      puts d.name
+    end
+  end
 end
